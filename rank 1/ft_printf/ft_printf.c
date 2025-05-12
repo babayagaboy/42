e1c76fe2c90c21c5e561c9012cd9dc26d5d3a5a0
Hugo Gutterres <hgutterr@c1r1s8.42lisboa.com>
2025-05-12 16:59:07 +0100

    ft_printf finished not evaluated

--- rank 1/ft_printf/ft_printf.c
+++ rank 1/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hgutterr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 16:29:31 by hgutterr          #+#    #+#             */
-/*   Updated: 2025/05/01 14:48:25 by hgutterr         ###   ########.fr       */
+/*   Updated: 2025/05/12 16:56:39 by hgutterr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,47 @@
 
 int	print_char(int c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
 int	print_str(char *str)
 {
-	int i;
+	int		i;
 
 	i = 0;
+	if (!str)
+		return (write(1, "(null)", 6));
 	while (str[i])
 		i += print_char(str[i]);
-	return(i);
+	return (i);
 }
 
 int	print_digit(long nbr, int base, char type)
 {
-	int len;
-	char *base_char;
+	int		len;
+	char	*base_char;
 
+	len = 0;
+	if (type == 'u')
+		nbr = (nbr) * (nbr > 0) - (nbr) * (nbr < 0);
+	base_char = "0123456789abcdef";
 	if (type == 'X')
 		base_char = "0123456789ABCDEF";
-	else
-		base_char = "0123456789abcdef";
-	len = 0;
 	if (nbr < 0)
-	{
-		print_char('-');
-		return(print_digit(-nbr, base, type) + 1); //+1 for the menos
-	}
+		return (print_char('-'), print_digit(-nbr, base, type) + 1);
 	else if (nbr < base)
-		return(print_char(base_char[nbr]));
+		return (print_char(base_char[nbr]));
 	else
 	{
 		len = print_digit(nbr / base, base, type);
-		return(len + print_digit(nbr % base, base, type));
+		return (len + print_digit(nbr % base, base, type));
 	}
 }
 
 int	print_format(char type, va_list ap)
 {
-	int	len;
+	int		len;
+	void	*p;
 
 	len = 0;
 	if (type == 'c')
@@ -62,14 +63,18 @@ int	print_format(char type, va_list ap)
 		len += print_str(va_arg(ap, char *));
 	else if (type == 'd' || type == 'i')
 		len += print_digit((long) va_arg(ap, int), 10, type);
-	else if (type == 'x')
+	else if (type == 'x' || type == 'X')
 		len += print_digit((long) va_arg(ap, unsigned int), 16, type);
-	else if (type == 'X')
-		len += print_digit((long) va_arg(ap, unsigned int), 16, type);
-	/*	else if (type == 'p')
-		len += print_p(va_arg(ap, void *));
+	else if (type == 'p')
+	{
+		p = va_arg(ap, void *);
+		if (!p)
+			return (write(1, "(nil)", 5));
+		len += print_str("0x");
+		len += print_digit((unsigned long long) p, 16, type);
+	}
 	else if (type == 'u')
-		len += print_char(va_arg(ap, int)); */
+		len += print_digit(va_arg(ap, unsigned int), 10, type);
 	else
 		len += print_char('%');
 	return (len);
@@ -77,14 +82,16 @@ int	print_format(char type, va_list ap)
 
 int	ft_printf(const char *str, ...)
 {
-	va_list ap;
-	int len;
+	va_list		ap;
+	int			len;
 
+	if (!str)
+		return (-1);
 	va_start(ap, str);
 	len = 0;
 	while (*str)
 	{
-		if(*str == '%')
+		if (*str == '%')
 			len += print_format(*(++str), ap);
 		else
 			len += print_char(*str);
@@ -94,11 +101,33 @@ int	ft_printf(const char *str, ...)
 	return (len);
 }
 
-int main()
+/* int main()
 {
-	int len = ft_printf("Hello %z\n", "Hugo");
+	int len = ft_printf("Hello %%\n");
 	ft_printf("string has %d char\n", len);
 
-	len = printf("Hello %z\n", "Hugo");
+	len = printf("Hello %%\n");
 	printf("string has %d char\n", len);
-}
+
+	unsigned int a = -52;
+	char *p = "h";
+	len = printf("%d -> %x -> %X -> %u; %p\n", -52, -52, -52, a, p);
+	printf("len = %d\n", len);
+	len = ft_printf("%d -> %x -> %X -> %u; %p\n", -52, -52, -52, a, p);
+	printf("len = %d\n", len);
+
+	char *s1 = NULL;
+	len = printf("%s\n", s1);
+	printf("len = %d\n", len);
+	char *s2 = NULL;
+	len = ft_printf("%s\n", s2);
+	printf("len = %d\n", len);
+
+
+	char *p1 = NULL;
+	len = printf("%p -> %p\n", p1, p);
+	printf("len = %d\n", len);
+	char *p2 = NULL;
+	len = ft_printf("%p -> %p\n", p2, p);
+	printf("len = %d\n", len);
+} */
--- rank 1/ft_printf/ft_printf.h
+++ rank 1/ft_printf/ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: hgutterr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 16:37:16 by hgutterr          #+#    #+#             */
-/*   Updated: 2025/04/24 16:37:18 by hgutterr         ###   ########.fr       */
+/*   Updated: 2025/05/12 16:57:10 by hgutterr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINT_H
-# define FT_PRINT_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdio.h>
 # include <unistd.h>
 # include <stdio.h>
 # include <stdarg.h>
 
-# endif
+#endif
