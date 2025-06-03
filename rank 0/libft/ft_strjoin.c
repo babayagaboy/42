c30d4f1b6110599580983bd39fdd509e85a19135
babayagaboy <hugo.guterres.2005@gmail.com>
2025-06-03 23:34:28 +0100

    fdf

--- rank 0/libft/ft_strjoin.c
+++ rank 0/libft/ft_strjoin.c
@@ -36,13 +36,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*newstr;
 	char	*s1_ptr;
 	char	*s2_ptr;
-	int		i;
 
 	if (!s1 || !s2)
 		return (NULL);
 	s1_ptr = (char *)s1;
 	s2_ptr = (char *)s2;
-	i = 0;
 	newstr = malloc(ft_strlen(s1_ptr) + ft_strlen(s2_ptr) + 1);
 	if (!newstr)
 		return (NULL);
--- rank 1/get_next_line/get_next_line.c
+++ rank 1/get_next_line/get_next_line.c
@@ -112,20 +112,21 @@ char	*get_next_line(int fd)
 	return (s1);
 }
 
-/* int	main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	int	fd;
 	char	*s1 = "";
 
 	(void) argc;
 	fd = open(argv[1], O_RDONLY);
+	int i = 0;
 	while (s1 && argv[1])
 	{
 		s1 = get_next_line(fd);
 		if (!s1)
 			break ;
-		printf("%s", s1);
+		printf("%d: %s",i++, s1);
 		free(s1);
 	}
 	close(fd);
-}*/
+}
